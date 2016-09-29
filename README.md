5419b2753dd301e69fea37323f3ace33f55ac0cf
niksinfinite10 <nikhil.surati@gmail.com>
2016-09-28 20:53:15 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#React.js Demo
+
+- ES6
+
+- React
+
+- Less
+
+- Webpack
+
+#How
+
+```
+$ npm install
+
+$ npm run start
+```
---
+++ src/components/App/App.jsx
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+
+
+let sampleData
+export default class App extends Component {
+
+   constructor(props){
+     super();
+     this.state = {inputText:undefined };
+     this.sampleData=undefined;
+   }
+
+   componentDidMount(){
+     this.sampleData = {
+     "main": [
+       {
+         "de": {
+         "identity": {
+           "version": {
+             "_number": "$Revision: 12293 $",
+             "_cldrVersion": "29"
+           },
+           "language": "de"
+         },
+         "delimiters": {
+           "quotationStart": "„",
+           "quotationEnd": "“",
+           "alternateQuotationStart": "‚",
+           "alternateQuotationEnd": "‘"
+         }
+       }
+     },
+       {"fr": {
+        "identity": {
+          "version": {
+            "_number": "$Revision: 12379 $",
+            "_cldrVersion": "29"
+          },
+          "language": "fr"
+        },
+        "delimiters": {
+          "quotationStart": "«",
+          "quotationEnd": "»",
+          "alternateQuotationStart": "«",
+          "alternateQuotationEnd": "»"
+        }
+      }},
+      {"it": {
+         "identity": {
+           "version": {
+             "_number": "$Revision: 12293 $",
+             "_cldrVersion": "29"
+           },
+           "language": "it"
+         },
+         "delimiters": {
+           "quotationStart": "«",
+           "quotationEnd": "»",
+           "alternateQuotationStart": "“",
+           "alternateQuotationEnd": "”"
+         }
+       }}
+
+     ]
+   };
+
+   this.setState({data:this.sampleData});
+
+   }
+
+
+  checkData(){
+    let searchValue = this.searchBar.value;
+    // console.log('sample data ',this.state.data);
+    console.log('sample data ',this.sampleData);
+    let result = "";
+    this.sampleData.main.map((record)=>{
+      // console.log('this is start',record);
+      for(let x in record){
+        if(record[x].delimiters.hasOwnProperty(searchValue))
+          result += x+'='+record[x].delimiters[searchValue]+",";
+        console.log(result);
+      }
+
+    });
+  }
+
+  addInput(){
+
+
+  }
+
+  shouldComponentUpdate(newProps, newState) {
+      console.log('shouldComponentUpdate');
+      this.renderOptions();
+       return this.state !== newState;
+
+    }
+
+  renderOptions(){
+      let items = [];
+    if(this.state.data){
+      this.state.data.main.map((record)=>{
+        for(let x in record){
+          items.push(<option key={x}> {record[x].delimiters} </option>);
+        }
+
+      });
+    }
+    else {
+      items.push(<option key="1" >no data found</option>);
+    }
+    return items;
+  }
+
+  componentWillUpdate(){
+    this.renderOptions();
+    console.log('componentWillUpdate');
+
+  }
+
+
+  render() {
+
+    return (
+      <div>
+      <h1>Property name is case sensitive</h1>
+        <input name="inputBox" type="text"  ref={(ref) => this.searchBar = ref} onChange={ this.checkData.bind(this)} />
+        <select>
+          {this.renderOptions()
+          }
+        </select>
+          <input type="button" value="+" onClick={this.addInput.bind(this)} />
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
